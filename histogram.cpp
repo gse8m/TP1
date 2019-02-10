fdc81ea6a8756ea6add8f19be6ec59e76592b224
Joel ZHAO <15339102561@163.com>
2019-02-11 01:17:07 +0700

    zhao

--- histogram.cpp
+++ histogram.cpp
@@ -1,12 +1,12 @@
-//
+// Copyright [2019] <SUN Hao, LIU Bohua, ZHAO Hairen>
 // header-start
 //////////////////////////////////////////////////////////////////////////////////
 //
-// \file      mean_and_median.cpp
+// \file      histogram.cpp
 //
 // \brief     This file belongs to the C++ tutorial project
 //
-// \author    Bernard
+// \author    SUN, LIU, ZHAO
 //
 // \copyright Copyright ng2goodies 2015
 //            Distributed under the MIT License
@@ -14,19 +14,19 @@
 //
 //////////////////////////////////////////////////////////////////////////////////
 // header-end
-//;;;
+//
 
 
 // C++ version
-// Compilation with g++ 5.3.0 & g++ 6.3.0 
+// Compilation with g++ 5.3.0 & g++ 6.3.0
 // mingwin: g++ -std=c++14 -O3 -o mean_and_median mean_and_median.cpp
 
 // Compilation with g++ 4.9.3
 // cygwin:  g++ -D_GLIBCXX_USE_C99 -D_GLIBCXX_USE_C99_DYNAMIC -std=c++14 -O3 ...
 
-// Other tool chain 
+// Other tool chain
 // msvc:    ok!
-// clang:   ok! 
+// clang:   ok!
 
 
 #include <string>
@@ -37,6 +37,8 @@
 
 using std::string;
 using std::vector;
+using std::count_if;
+
 
 // This simple program reads from a file a set of numbers (double format)
 // computes a running mean value, computes the median after sort
@@ -66,4 +68,35 @@ int main(int argc, char *argv[]) {
   std::cout << "number of elements = " << buf.size()
             << ", median = " << median
             << ", mean = " << mean << std::endl;
+
+
+
+  vector<int> bin;
+  for (int b = 0; b < 8000; b += 100) {
+    int c = b + 100;
+    int num = 0;
+    num = count_if(buf.begin(), buf.end(), [&](int i){return i >= b && i < c;});
+    bin.push_back(num);
+  }
+  int max = 0;
+  for (double j = 0; j < bin.size(); ++j) {
+    if (bin[j]> max) {
+      max = bin[j];
+    }
+  }
+  int a = 0;
+  for (double j = 0; j< bin.size(); ++j) {
+    std::cout.setf(std::ios::right);
+    std::cout.width(6);
+    std::cout << a;
+    std::cout.setf(std::ios::right);
+    std::cout.width(8);
+    std::cout << bin[j] << "  ";
+    for (int etoile = 0; etoile < 60 * bin[j] / max; etoile++) {
+      std::cout << "*";
+    }
+    std::cout << std::endl;
+    a = a + 100;
+  }
+  return EXIT_SUCCESS;
 }
